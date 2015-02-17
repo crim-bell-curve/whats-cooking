9d6600f18a723776f66f676440f9097d2b3633f9
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-02-17 01:24:38 -0500

    add log

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-/*
+/* 
  * Server for fetching the current lunch menu url for both dining halls
  * and then scraping those menus
- *
+ * 
  * To do
  *   - scrape once a day at the first request and cache the 
  *     menu object returned by scraper.js for faster loading
- *    
- *
+ * 
  */
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var moment = require('moment');
 
+var log = JSON.parse(String(fs.readFileSync('./data/log.json')));
+var today = moment().format('dddd').toLowerCase();
+if (today !== log.current) {
+	// scrape today's data
+	log.current = today;
+	fs.writeFile(__dirname + '/data/log.json', JSON.stringify(log))	
+
+}
 app.use(express.static(__dirname + "/"));
 
 app.get('/', function (req, res) {
@@ -21,8 +30,6 @@ app.get('/', function (req, res) {
 var port = process.env.PORT || 5000
 
 var server = app.listen(port, function () {
-
     var host = server.address().address
     console.log('Example app listening at http://%s:%s', host, port)
-
 })
