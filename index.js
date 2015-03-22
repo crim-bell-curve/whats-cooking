db843b3b98f33a2c50b7585f611e07873cc2cbcf
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-03-22 13:44:10 -0400

    remove junk and add unavailable

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ window.addEventListener('load', function() {
   d3.select('#day')
     .text(today)
   var colors = ['#BEA8E6', '#84C3C5', '#BD7B99', '#40586F']
-  function display(menu) {
+  function display(menu, name) {
     var content = d3.select('#content');
-    if (!menu) {
-      content.append('p').text('NOT AVAILABLE. THANKS SODEXO...')
-    }
     var count = 0
+    content.append('h1')
+      .text(name)
+      .style({'text-align': 'center', 'font-size': 50, 'background-color': colors[count++]})
     for (var meal in menu) {
       content.append('div')
         .attr('class', 'meal')
@@ -23,8 +23,8 @@ window.addEventListener('load', function() {
       for (var station in menu[meal]) {
         stat = station.replace(/\//g, '');
         m.append('div')
-         .attr('class', meal)
-         .attr('id', stat)
+          .attr('class', meal)
+          .attr('id', stat)
         var s = d3.select('#' + stat)
         s.append('p')
           .text(station)
@@ -36,8 +36,17 @@ window.addEventListener('load', function() {
     }
   }
 
+  if (typeof sadler !== 'undefined') {
+    display(sadler, 'Sadler')
+  } else {
+    d3.select('#content')
+      .append('p')
+      .text('SADLER NOT AVAILABLE. THANKS SODEXO...')
+      .style({'font-size': 50, 'text-align': 'center', 'background-color': '#000', 'color': '#fff'})
+  }
+
   if (typeof caf !== 'undefined') {
-    display(caf)
+    display(caf, 'Caf')
   } else {
     d3.select('#content')
       .append('p')
@@ -45,12 +54,4 @@ window.addEventListener('load', function() {
       .style('font-size', 50)
   }
 
-  if (typeof sadler !== 'undefined') {
-    display(sadler)
-  } else {
-    d3.select('#content')
-      .append('p')
-      .text('SADLER NOT AVAILABLE. THANKS SODEXO...')
-      .style('font-size', 50)
-  }
 });
